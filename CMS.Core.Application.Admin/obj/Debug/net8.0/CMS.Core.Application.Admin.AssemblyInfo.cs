ef8671712acf6a0d9b91d3ede81a2a5e4cd19f3b
Raouf Ebrahimi Nasab <r.ibrahiminasab@gmail.com>
2024-05-18 12:55:52 +0330

    test

--- CMS.Core.Application.Admin/obj/Debug/net8.0/CMS.Core.Application.Admin.AssemblyInfo.cs
+++ CMS.Core.Application.Admin/obj/Debug/net8.0/CMS.Core.Application.Admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Core.Application.Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49164e4d68f04ce3ba53d1c28adfde9ede956c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92e29a9f602d4cdb132a74320fa28f04d98cf81")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Core.Application.Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Core.Application.Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
